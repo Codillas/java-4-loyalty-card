e3074c3625a5b7a6666a7db568355dff9fa4d955
Anton Lappa <lappaantony@gmail.com>
2026-07-23 17:42:20 +0200

    service-layer (#2)

--- src/main/java/loyaltycard/controller/AdminController.java
+++ src/main/java/loyaltycard/controller/AdminController.java
@@ -1,5 +1,35 @@
 package loyaltycard.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import loyaltycard.controller.dto.AdminDto;
+import loyaltycard.controller.dto.SignUpRequestDto;
+import loyaltycard.mapper.AdminMapper;
+import loyaltycard.service.AdminService;
+import loyaltycard.service.model.Admin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.UUID;
+
+
+@RestController
+@RequestMapping("/admins")
+@RequiredArgsConstructor
+public class AdminController {
+
+
+    private final AdminService adminService;
+    private final AdminMapper adminMapper;
+
+
+    @PostMapping
+    public ResponseEntity<AdminDto> createAdmin(@Valid @RequestBody SignUpRequestDto signUpRequestDto) {
+
+        Admin admin = adminMapper.toDomain(signUpRequestDto);
+        Admin createdAdmin = adminService.createAdmin(admin);
+        AdminDto adminDto = adminMapper.toDto(createdAdmin);
 import loyaltycard.controller.dto.AdminDto;
 import loyaltycard.controller.dto.Role;
 import loyaltycard.controller.dto.SignUpRequestDto;
@@ -37,6 +67,12 @@ public class AdminController {
      return ResponseEntity.ok().body(adminDto);
     }
 
+    @GetMapping
+    public ResponseEntity<List<AdminDto>> getAllAdmins() {
+
+        List<Admin> adminList = adminService.getAdmins();
+
+        List<AdminDto> adminDtoList = adminList.stream().map(adminMapper::toDto).toList();
     @GetMapping("/admins")
     public ResponseEntity<List<AdminDto>> getAllAdmins() {
 
@@ -45,6 +81,39 @@ public class AdminController {
         return ResponseEntity.ok().body(adminDtoList);
     }
 
+    @GetMapping("/{adminId}")
+    public ResponseEntity<AdminDto> getAdminById(@PathVariable UUID adminId) {
+
+        Admin admin = adminService.getAdminById(adminId);
+
+        AdminDto adminDto = adminMapper.toDto(admin);
+
+        return ResponseEntity.ok().body(adminDto);
+
+    }
+
+    @PutMapping("/{adminId}/activate")
+    public ResponseEntity<AdminDto> activateAdmin(@PathVariable UUID adminId) {
+
+        Admin admin = adminService.activateAdmin(adminId);
+
+        AdminDto adminDto = adminMapper.toDto(admin);
+
+        return ResponseEntity.ok().body(adminDto);
+
+    }
+
+    @PutMapping("/{adminId}/block")
+    public ResponseEntity<AdminDto> blockAdmin(@PathVariable UUID adminId) {
+
+        Admin admin = adminService.blockAdmin(adminId);
+
+        AdminDto adminDto = adminMapper.toDto(admin);
+
+        return ResponseEntity.ok().body(adminDto);
+    }
+
+}
 }
 
 
